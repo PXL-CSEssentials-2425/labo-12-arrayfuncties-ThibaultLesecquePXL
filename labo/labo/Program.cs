3ce39ee5ca30caabe1f6a269d2638e6297911c77
ThibaultLesecquePXL <thibault.lesecque@student.pxl.be>
2024-11-22 09:05:11 +0100

    Deel 1 - Versie 1.0

---
+++ labo/labo/Program.cs
@@ -0,0 +1,15 @@
+﻿string[] names = { "Peter", "Tom", "Piet", "Ahmed", "Benny", "Anna", "Jane", "Ali", "John", "Tom" };
+Console.WriteLine("Geef een naam");
+string input = Console.ReadLine();
+
+int index = Array.IndexOf(names, input);
+
+if (index == -1)
+{
+    Console.WriteLine($"De naam {input} staat op plaats {index + 1}");
+}
+else
+{
+    Console.WriteLine($"De naam {input} staat niet op de lijst {index + 1}");
+}
+Console.ReadLine();  //deel 1
