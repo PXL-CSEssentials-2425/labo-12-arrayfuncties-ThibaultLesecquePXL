e7ddaae1c45dfa3b08af46bad74f048f952e37d8
ThibaultLesecquePXL <thibault.lesecque@student.pxl.be>
2024-11-22 09:27:52 +0100

    Deel 2 - Versie 1.0

--- labo/labo/Program.cs
+++ labo/labo/Program.cs
@@ -4,12 +4,33 @@ string input = Console.ReadLine();
 
 int index = Array.IndexOf(names, input);
 
-if (index == -1)
+if (index != -1)
 {
     Console.WriteLine($"De naam {input} staat op plaats {index + 1}");
 }
 else
 {
-    Console.WriteLine($"De naam {input} staat niet op de lijst {index + 1}");
+    Console.WriteLine($"De naam {input} staat niet op de lijst");
 }
-Console.ReadLine();  //deel 1
+
+Console.WriteLine("\nGeef een letter: ");
+string letter = Console.ReadLine().ToUpper();
+
+if (!string.IsNullOrEmpty(letter) && char.IsLetter(letter[0]))
+{
+    string[] filterednames = Array.FindAll(names, name => name.ToUpper().StartsWith(letter.ToUpper()));
+    if (filterednames.Length > 0)
+    {
+        Console.WriteLine($"Namen die beginnen met de letter {letter}: {string.Join(", ", filterednames)}");
+    }
+    else
+    {
+        Console.WriteLine($"Er zijn geen namen die beginnen met de letter {letter}");
+    }
+}
+else
+{
+    Console.WriteLine("Voer een geldige letter in");
+}
+
+Console.ReadLine();
